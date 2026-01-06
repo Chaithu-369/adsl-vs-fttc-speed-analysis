4c50d17c69ad04072d92a62abbd1898b6a3e27ad
Adithya1315 <at24aer@herts.ac.uk>
2026-01-06 20:01:46 +0000

    Cleaning: filter ADSL and FTTC, remove NA
    Cleaning: filter ADSL and FTTC, remove NA

--- SS20251221_updated_software.R
+++ SS20251221_updated_software.R
@@ -5,3 +5,15 @@ data <- read_excel("-17.csv.xlsx")
 names(data) <- trimws(names(data))
 
 str(data)
+
+data_clean <- data %>%
+  select(Technology, DownloadSpeed = t24hr) %>%
+  filter(!is.na(Technology), !is.na(DownloadSpeed)) %>%
+  filter(Technology %in% c("ADSL","FTTC"))
+
+data_clean$Technology <- factor(data_clean$Technology,
+                                levels=c("ADSL","FTTC"))
+
+data_clean$DownloadSpeed <- as.numeric(data_clean$DownloadSpeed)
+
+print(table(data_clean$Technology))
